39a49599a186eb4916f0fa7a1ff0de09f00c7d97
MatheusZamo <19matheushenrique03@gmail.com>
2024-11-14 23:06:59 -0300

    using localforage to persist data

--- src/hooks/use-items.js
+++ src/hooks/use-items.js
@@ -1,8 +1,26 @@
-import { useState } from "react"
+import localforage from "localforage"
+import { useState, useEffect } from "react"
 
 const useItems = () => {
   const [items, setItems] = useState([])
   const [orderBy, setOrderBy] = useState("newest")
+
+  useEffect(() => {
+    localforage
+      .setItem("saveThings", items)
+      .catch((error) => alert(error.message))
+  }, [items])
+
+  useEffect(() => {
+    localforage
+      .getItem("saveThings")
+      .then((value) => {
+        if (value) {
+          setItems(value)
+        }
+      })
+      .catch((error) => alert(error.message))
+  }, [])
 
   const handleSubmitForm = (newItem) => setItems((prev) => [...prev, newItem])
 
